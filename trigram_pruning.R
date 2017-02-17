84c71b8194ce154b5473ada1e23272038859a66b
oscarvilla <villaoscarad@gmail.com>
2017-02-17 00:10:23 -0500

    working out with parLapply and 10M rows. Will try with 26+M rows.

--- trigram_pruning.R
+++ trigram_pruning.R
@@ -3,49 +3,27 @@ setwd("~/Documents/finalCapstone/datasets/train")
 trigram <- as.data.table(readRDS("./trigramRDS")$terms)
 
 library(parallel)
+
 # Calculate the number of cores
 no_cores <- detectCores() - 1
 
 # Initiate cluster
-library(foreach)
-library(doParallel)
-## Activate 3 clusters for parallelizing the work
-cl <- makeCluster(3)
-registerDoParallel(cl)
-makeForkCluster(nnodes = getOption("mc.cores", 3L))
+cl <- makeCluster(no_cores)
 
-x <- trigram
+x <- trigram[1:10000000]
 
 m <- nrow(x) * 3 - 1
 n <- nrow(x) * 3
-x$root <- parLapply(x, function(x) paste(unlist(strsplit(as.character(x), split = "_"))[seq(1, m, 3)], 
-                                      unlist(strsplit(as.character(x), split = "_"))[seq(2, m, 3)]))
+x$root <- parLapply(cl, x, function(x) paste(unlist(strsplit(as.character(x), split = "_"))[seq(1, 29999999, 3)], 
+                                      unlist(strsplit(as.character(x), split = "_"))[seq(2, 30000000, 3)], 
+                                      sep = "_"))
 
 stopCluster(cl)
 
-
-
+####################################################################################################
 root <- foreach(i=1:8, .combine = rbind, .packages = c("data.table")) %dopar% {
         k <- x[t==i, c("V1"), with = FALSE]
         root <- paste(apply(k, 1, FUN = function(x) strsplit(as.character(x), split = "_")[[1]][1]), 
                       apply(k, 1, FUN = function(x) strsplit(as.character(x), split = "_")[[1]][2]))
 }
-
-
- 
-
-#trigram <- as.data.table(readRDS("./trigramRDS"))
-
-
-## verify cores
-getDoParWorkers()
-
-
-x$root <- as.vector(root)
-
-
-trigram <- cbind(trigram, root = root)
-
-t2 <- Sys.time()
-t2-t1
-stopCluster(cl)
+####################################################################################################
